83f7c7c740eb3798ad3b797d26006c12276f95fd
kiiroisenko786 <projectyuni5@gmail.com>
2025-08-24 00:34:32 +0100

    about page and ontact page update

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0973a4ae8b2be182504c9ca23bfcef78396421fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+976c7d2b6426d3fd18c96a739f6204de8eca2584")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
